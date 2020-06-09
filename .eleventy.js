d64f4be93eed6e6c0bc03da355715e45b2707cf1
Maruan Sahyoun <sahyoun@fileaffairs.de>
2020-06-09 19:59:01 +0200

    PDFBOX-4848: enable handling of static assets with Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,13 @@
 module.exports = function(eleventyConfig) {
 
+  // copy static files to output
+  eleventyConfig.addPassthroughCopy("./content/bootstrap");
+  eleventyConfig.addPassthroughCopy("./content/css/*.css");
+  eleventyConfig.addPassthroughCopy("./content/images");
+  eleventyConfig.addPassthroughCopy("./content/.htaccess");
+  eleventyConfig.addPassthroughCopy("./content/doap_PDFBox.rdf");
+  eleventyConfig.addPassthroughCopy("./content/download.cgi");
+
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("./content/_posts/*.md");
   });
