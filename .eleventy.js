2aec28e89980bb50173a357d8cff83eb69792aec
Maruan Sahyoun <sahyoun@fileaffairs.de>
2020-06-10 08:05:12 +0200

    PDFBOX-4848: enable generation of anchor tags in headings

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 module.exports = function(eleventyConfig) {
 
+  // enable generation of header anchor tags
+  // needs the markdown-it-anchor plugin
+  const markdownIt = require("markdown-it");
+  const markdownItAnchor = require("markdown-it-anchor");
+  const markdownLib = markdownIt().use(markdownItAnchor);
+  eleventyConfig.setLibrary("md", markdownLib);
+
   // copy static files to output
   eleventyConfig.addPassthroughCopy("./content/bootstrap");
   eleventyConfig.addPassthroughCopy("./content/css/*.css");
