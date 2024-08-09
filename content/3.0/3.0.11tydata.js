d2ee9c9eb22834f5455abae8dc72602d5bea9a1c
Andreas Lehmkühler <andreas@lehmi.de>
2024-08-09 08:09:15 +0200

    update website due to the 3.0.3 release

--- content/3.0/3.0.11tydata.js
+++ content/3.0/3.0.11tydata.js
@@ -1,6 +1,6 @@
 module.exports = {
     eleventyComputed: {
-        release: '3.0.0-alpha3',
+        release: '3.0.3',
         eleventyNavigation: {
             version: '3.0',
             key: data => data.eleventyNavigation && data.eleventyNavigation.key ? data.eleventyNavigation.key : data.title,
